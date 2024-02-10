f1fd0ac9357ea2a57bd1cde8de4956a696e7e6c1
yossefsabry <yossefsabry@gmail.com>
2024-02-10 03:13:48 +0200

    adding component footer and updating navbar
    adding the component footer for the copywrite and some links and adding image to the links in navbar

--- src/components/MenuNavbar.jsx
+++ src/components/MenuNavbar.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const MenuNavbar = (props) => {
+  const [hovered, setHovered] = useState(false);
 
   const style__menu = {
     display: "flex",
     alignItems: "center",
     gap: "0px 10px",
     color: "white",
-    padding: "12px 0",
-    transititon: "200ms ease-in-out",
+    padding: `${props.imagePlaylist ? '6px 0px' : '12px 0'}`,
+    transition: "all .3s", // Added transition for background-color
     cursor: "pointer",
+    textDecoration: "none", // Remove default Link underline
+    transform: hovered ? "translateX(20px)" : "translateX(0)", // Background color changes when hovered
   };
 
   return (
-    <Link style={style__menu} to={props.url}>
-      <span style={{fontWeight: "100"}}>{props.icon}</span>
-      <p>{props.name}</p>{" "}
+    <Link
+      to={props.url}
+      style={style__menu}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <span style={{ fontWeight: "100" }}>{props.icon}</span>
+      {props.imagePlaylist ? (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '20px' }}>
+          <img src={props.image} alt="image playlist" style={{ width: '60px', height: '60px' }} />
+          <p>{props.name}</p>{" "}
+        </div>
+      ) : (
+          <p>{props.name}</p>
+        )}
     </Link>
   );
 };
 
 export default MenuNavbar;
+
